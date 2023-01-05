5c3c362ef62782555b3d6e7cd0a2b55112ddcbc1
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2023-01-05 18:24:58 +0100

    cuts, parallelism

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate ndarray;
 extern crate quick_xml;
 extern crate base64;
 extern crate byteorder;
+extern crate crossbeam;
 
 use quick_xml::events::BytesText;
 use quick_xml::writer::Writer;
@@ -11,36 +12,51 @@ use resvg::tiny_skia;
 use ndarray::prelude::*;
 use ndarray::{Array3};
 use byteorder::{ByteOrder, LittleEndian};
+use std::sync::{Mutex};
 
-const WIDTH: u32 = 60;
+const WIDTH: u32 = 150;
+const DEPTH: u32 = 50;
 const HEIGHT_OFFSET: usize = 2;
 const HEIGHT_OFFSET32: u32 = HEIGHT_OFFSET as u32;
-const DEPTH: u32 = 20;
-const ALPHA_THRESHOLD: u8 = 50;
-const STRENGTH_DECAY: f64 = 0.85;
-const COUNT_DECAY: f64 = 0.05;
-const FINAL_STRENGTH_DIFF_DECAY: f64 = 0.5;
+const ALPHA_THRESHOLD: u8 = 128;
+const STRENGTH_DECAY: f64 = 0.6;
+const COUNT_DECAY: f64 = 0.6;
+const FINAL_STRENGTH_DIFF_START: f64 = 2.0;
+const FINAL_STRENGTH_DIFF_DECAY: f64 = 0.6;
+const CUT_WIDTH: f64 = 0.3;
+const MAX_POSTFILL_DISTANCE: usize = 5;
+const MAX_OTHER_COUNT: usize = 2;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Constraint {
     RedMust,
     RedArea,
+    RedPostfill,
     BlueMust,
     BlueArea,
+    BluePostfill,
     Mustnt,
     Any,
+    RedMustnt,
+    BlueMustnt,
 }
+
+// cervena nepojde do RedMustnt ale v BlueMustnt je vzdy (nema jej tam co konkurovat).
+// pozor ale na to ze tadial moze robit tunely.
+// riesenie:
+// - Red sa moze v BlueMustnt sirit iba horizontalne
+// - Zaver: BlueMustnt sa povazuje za Red ak je aspon na jednom jeho horizontalnom okraji Red
 
 use Constraint::*;
 
 fn change_path_width(tree: &mut usvg::Tree, width: f64) {
-    let path = tree.node_by_id("path1326").unwrap();
+    let path = tree.node_by_id("the_path").unwrap();
     match &mut *path.borrow_mut() {
         usvg::NodeKind::Path(path) => {
             path.stroke.as_mut().unwrap().width =
                 unsafe{usvg::NonZeroPositiveF64::new_unchecked(width)};
         },
-        _ => panic!("path1175 is not a path"),
+        _ => panic!("the_path is not a path"),
     };
 }
 
@@ -56,7 +72,7 @@ fn apply_path(
     let mut pixmap = tiny_skia::Pixmap::new(WIDTH, height as u32).unwrap();
     resvg::render(
         &tree,
-        usvg::FitTo::Size(WIDTH, (*out).len() as u32),
+        usvg::FitTo::Size(WIDTH, height as u32),
         tiny_skia::Transform::default(),
         pixmap.as_mut(),
     ).unwrap();
@@ -91,6 +107,46 @@ fn apply_path(
     }
 }
 
+fn apply_cut_path(
+    tree: &mut usvg::Tree,
+    out: &mut Array3<Constraint>,
+    is_blue: bool,
+) {
+    change_path_width(tree, CUT_WIDTH);
+
+    let tree_size = tree.size;
+    let tree_height: f32 = tree_size.height() as f32;
+    let tree_width: f32 = tree_size.width() as f32;
+
+    let height = (*out).dim().1;
+    let mut pixmap = tiny_skia::Pixmap::new(WIDTH, DEPTH).unwrap();
+    resvg::render(
+        &tree,
+        usvg::FitTo::Original,
+        tiny_skia::Transform::from_scale(WIDTH as f32 / tree_width, (DEPTH + 1) as f32 / tree_height),
+        pixmap.as_mut(),
+    ).unwrap();
+
+    for x in 0..WIDTH {
+        let xsz = x as usize;
+        for z in 0..DEPTH {
+            let zsz = z as usize;
+            if pixmap.pixel(x, z).unwrap().alpha() > ALPHA_THRESHOLD {
+                for y in 0..height {
+                    let old = out[(xsz, y, zsz)];
+                    out[(xsz, y, zsz)] =
+                        if old == Mustnt { Mustnt }
+                        else if is_blue {
+                            if old == RedMustnt { Mustnt } else { BlueMustnt }
+                        } else {
+                            if old == BlueMustnt { Mustnt } else { RedMustnt }
+                        };
+                }
+            }
+        }
+    }
+}
+
 type Dim = (usize, usize, usize);
 
 fn main() {
@@ -100,8 +156,14 @@ fn main() {
     let svg_data = std::fs::read("resources/wire.svg").unwrap();
     let mut tree = usvg::Tree::from_data(&svg_data, &opt).unwrap();
 
-    let svg_data1 = std::fs::read("resources/wire1.svg").unwrap();
-    let mut tree1 = usvg::Tree::from_data(&svg_data1, &opt).unwrap();
+    let svg_data = std::fs::read("resources/wire1.svg").unwrap();
+    let mut tree1 = usvg::Tree::from_data(&svg_data, &opt).unwrap();
+
+    let svg_data = std::fs::read("resources/predel.svg").unwrap();
+    let mut tree_cut = usvg::Tree::from_data(&svg_data, &opt).unwrap();
+
+    let svg_data = std::fs::read("resources/predel1.svg").unwrap();
+    let mut tree_cut1 = usvg::Tree::from_data(&svg_data, &opt).unwrap();
 
     // get raster height
     let tree_size = tree.size;
@@ -125,6 +187,9 @@ fn main() {
     constraints.slice_mut(s![.., 0..2, ..]).fill(BlueMust);
     constraints.slice_mut(s![.., height - 2 .. height, ..]).fill(RedMust);
 
+    apply_cut_path(&mut tree_cut, &mut constraints, false);
+    apply_cut_path(&mut tree_cut1, &mut constraints, true);
+
     let add_neighbours = |pos: Dim, target: &mut Vec<Dim>| {
         let (xm, ym, zm) = pos;
         if xm > 0 {
@@ -147,11 +212,25 @@ fn main() {
         }
     };
 
+    let add_horizontal_neighbours = |pos: Dim, target: &mut Vec<Dim>| {
+        let (xm, ym, zm) = pos;
+        if xm > 0 {
+            target.push((xm - 1, ym, zm));
+        }
+        if xm < width - 1 {
+            target.push((xm + 1, ym, zm));
+        }
+        if zm > 0 {
+            target.push((xm, ym, zm - 1));
+        }
+        if zm < depth - 1 {
+            target.push((xm, ym, zm + 1));
+        }
+    };
+
     // A*
     {
-        for _ in 0..3 {
-            let mut astar_raster = Array3::from_elem(dims, 0.0);
-
+        for iteration in 0..2 {
             // Count generators
             let mut origin_count = 0;
             for x in 0..(WIDTH as usize) {
@@ -165,74 +244,101 @@ fn main() {
                 }
             }
 
+            let mut jobs = vec![];
+
             // Perform A*
-            let mut processed_origin_count = 0;
             for x in 0..width {
                 for y in 0..height {
                     for z in 0..depth {
-                        for (generator, constr1, constr2, signum) in [
-                            (BlueMust, RedMust, RedArea, 1.0),
-                            (RedMust, BlueMust, BlueArea, -1.0),
+                        for (generator, constr1, constr2, signum, my_mustnt, other_postfill, other_mustnt, my_postfill) in [
+                            (BlueMust, RedMust, RedArea, 1.0, BlueMustnt, RedPostfill, RedMustnt, BluePostfill),
+                            (RedMust, BlueMust, BlueArea, -1.0, RedMustnt, BluePostfill, BlueMustnt, RedPostfill),
                         ] {
                             if constraints[(x, y, z)] == generator {
-                                let mut generator_astar_raster = Array3::from_elem(dims, 0.0);
-                                let mut final_strength = 1.0;
-                                let mut final_strength_diff = 2.0;
-                                let mut strength = 1.0;
-                                let mut visited = Array3::from_elem(dims, false);
-                                let mut todo_next = vec![(x, y, z)];
-                                let mut todo_next2 = vec![];
-                                let mut todo_next3 = vec![];
-                                // let mut todo_next4 = vec![];
-
-                                let mut last_count = 1.0;
-
-                                while !todo_next.is_empty() {
-                                    let todo = todo_next;
-                                    todo_next = todo_next2;
-                                    todo_next2 = todo_next3;
-                                    todo_next3 = vec![];
-                                    // todo_next4 = vec![];
-
-                                    let stranger_penalty = {STRENGTH_DECAY * STRENGTH_DECAY} / (last_count * last_count);
-                                    let mut count = 0;
-                                    for pos in todo {
-                                        if visited[pos] { continue; }
-                                        visited[pos] = true;
-                                        let constr = constraints[pos];
-                                        if constr == Mustnt || constr == constr1 || constr == constr2 {
-                                            final_strength *= 1.0 + final_strength_diff;
-                                        }
-                                        if constr == Mustnt { continue; }
-                                        if constr == constr1 { continue; }
-                                        if constr == constr2 {
-                                            generator_astar_raster[pos] += strength * stranger_penalty;
-                                            add_neighbours(pos, &mut todo_next3);
-                                        } else {
-                                            generator_astar_raster[pos] += strength;
-                                            add_neighbours(pos, &mut todo_next);
-                                            count += 1;
-                                        }
-                                    }
-                                    if count != 0 {
-                                        last_count = count as f64 * COUNT_DECAY + 1.0 * (1.0 - COUNT_DECAY);
-                                        strength *= STRENGTH_DECAY / last_count;
-                                        final_strength_diff *= FINAL_STRENGTH_DIFF_DECAY;
-                                    }
-                                }
-
-                                if processed_origin_count % 25 == 0 {
-                                    println!("origin_count {} / {}", processed_origin_count, origin_count);
-                                }
-                                processed_origin_count += 1;
-                                generator_astar_raster *= signum * final_strength;
-                                astar_raster += &generator_astar_raster;
+                                jobs.push((x, y, z, constr1, constr2, signum, my_mustnt, other_postfill, other_mustnt, my_postfill));
                                 break;
                             }
                         }
                     }
                 }
             }
+
+            let jobs = Mutex::new(jobs);
+            let mutexed = Mutex::new((Array3::from_elem(dims, 0.0), 0));
+
+            crossbeam::scope(|scope| {
+                for _ in 0..8 {
+                    scope.spawn(|_| {
+                        loop {
+                            let job = jobs.lock().unwrap().pop();
+                            match job {
+                                Some((x, y, z, constr1, constr2, signum, my_mustnt, other_postfill, other_mustnt, my_postfill)) => {
+                                    let mut generator_astar_raster = Array3::from_elem(dims, 0.0);
+                                    let mut final_strength = 1.0;
+                                    let mut final_strength_diff = FINAL_STRENGTH_DIFF_START;
+                                    let mut strength = 1.0;
+                                    let mut visited = Array3::from_elem(dims, false);
+                                    let mut todo_next = vec![(x, y, z)];
+                                    let mut todo_next2 = vec![];
+                                    let mut todo_next3 = vec![];
+                                    // let mut todo_next4 = vec![];
+
+                                    let mut last_count = 1.0;
+
+                                    while !todo_next.is_empty() {
+                                        let todo = todo_next;
+                                        todo_next = todo_next2;
+                                        todo_next2 = todo_next3;
+                                        todo_next3 = vec![];
+                                        // todo_next4 = vec![];
+
+                                        let stranger_penalty = {STRENGTH_DECAY * STRENGTH_DECAY} / (last_count * last_count);
+                                        let mut count = 0;
+                                        for pos in todo {
+                                            if visited[pos] { continue; }
+                                            visited[pos] = true;
+                                            let constr = constraints[pos];
+                                            if constr == Mustnt || constr == constr1 || constr == constr2 {
+                                                final_strength *= 1.0 + final_strength_diff;
+                                            }
+                                            if constr == Mustnt { continue; }
+                                            if constr == constr1 { continue; }
+                                            if constr == my_mustnt || constr == other_postfill { continue; }
+                                            if constr == other_mustnt || constr == my_postfill {
+                                                add_horizontal_neighbours(pos, &mut todo_next);
+                                            } else if constr == constr2 {
+                                                generator_astar_raster[pos] += strength * stranger_penalty;
+                                                add_neighbours(pos, &mut todo_next3);
+                                            } else {
+                                                generator_astar_raster[pos] += strength;
+                                                add_neighbours(pos, &mut todo_next);
+                                                count += 1;
+                                            }
+                                        }
+                                        if count != 0 {
+                                            last_count = (1.0 / count as f64) * COUNT_DECAY + 1.0 - COUNT_DECAY;
+                                            strength *= STRENGTH_DECAY * last_count;
+                                            final_strength_diff *= FINAL_STRENGTH_DIFF_DECAY;
+                                        }
+                                    }
+
+                                    generator_astar_raster *= signum * final_strength;
+
+                                    let (astar_raster, processed_origin_count) = &mut *mutexed.lock().unwrap();
+                                    if *processed_origin_count % 25 == 0 {
+                                        println!("origin_count {} {}/{}", iteration, processed_origin_count, origin_count);
+                                    }
+                                    *processed_origin_count += 1;
+                                    *astar_raster += &generator_astar_raster;
+                                },
+                                None => break,
+                            }
+                        }
+                    });
+                }
+            }).unwrap();
+
+            let (astar_raster, _) = mutexed.into_inner().unwrap();
 
             // Count areas
             for x in 0..width {
@@ -255,6 +361,78 @@ fn main() {
                                 };
                         } else if matches!(constr, RedMust | Mustnt) {
                             nonblue = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Final RedMustnt and BlueMustnt removal.
+    for y in 0..height {
+        for z in 0..depth {
+            'nextpt: for x in 0..width {
+                let constr = constraints[(x, y, z)];
+                if constr == RedMustnt {
+                    let mut visited = Array3::from_elem(dims, false);
+                    let mut todo_next = vec![(x, y, z)];
+                    let mut other_count = 0;
+                    for _ in 0..MAX_POSTFILL_DISTANCE {
+                        if todo_next.is_empty() {
+                            break;
+                        }
+                        let todo = todo_next;
+                        todo_next = vec![];
+                        let mut has_other = false;
+                        for pos in todo {
+                            if visited[pos] { continue; }
+                            visited[pos] = true;
+                            let constr2 = constraints[pos];
+                            if matches!(constr2, BlueMust | BlueArea) {
+                                constraints[(x, y, z)] = BluePostfill;
+                                continue 'nextpt;
+                            } else if matches!(constr2, RedMustnt | BluePostfill) {
+                                add_horizontal_neighbours(pos, &mut todo_next);
+                            } else {
+                                has_other = true;
+                            }
+                        }
+                        if has_other {
+                            other_count += 1;
+                            if other_count == MAX_OTHER_COUNT {
+                                break;
+                            }
+                        }
+                    }
+                } else if constr == BlueMustnt {
+                    let mut visited = Array3::from_elem(dims, false);
+                    let mut todo_next = vec![(x, y, z)];
+                    let mut other_count = 0;
+                    for _ in 0..MAX_POSTFILL_DISTANCE {
+                        if todo_next.is_empty() {
+                            break;
+                        }
+                        let todo = todo_next;
+                        todo_next = vec![];
+                        let mut has_other = false;
+                        for pos in todo {
+                            if visited[pos] { continue; }
+                            visited[pos] = true;
+                            let constr2 = constraints[pos];
+                            if matches!(constr2, RedMust | RedArea) {
+                                constraints[(x, y, z)] = RedPostfill;
+                                continue 'nextpt;
+                            } else if matches!(constr2, BlueMustnt | RedPostfill) {
+                                add_horizontal_neighbours(pos, &mut todo_next);
+                            } else {
+                                has_other = true;
+                            }
+                        }
+                        if has_other {
+                            other_count += 1;
+                            if other_count == MAX_OTHER_COUNT {
+                                break;
+                            }
                         }
                     }
                 }
